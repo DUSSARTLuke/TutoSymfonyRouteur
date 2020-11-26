5f2dfd37a2533a4a8a589130ae752223c4c4f08f
DUSSART Luke <lukedussart@hotmail.fr>
2020-11-26 09:51:34 +0100

    traduction

--- src/Controller/EmployeController.php
+++ src/Controller/EmployeController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Dompdf\Dompdf;
 
 /**
  * @Route("/employe/", name="employe_")
@@ -11,6 +13,23 @@ use Symfony\Component\Routing\Annotation\Route;
 class EmployeController extends AbstractController
 {
 
+  
+   
+  
+  
+  /**
+   * @Route("employe2/{nom}",
+   * name="employe_redirect",
+   * requirements = {"nom":"[A-Za-z]+"})
+   * 
+   */
+  public function redirectionV2(string $nom)
+  {
+    $nom = "Bond";
+    return $this->redirectToRoute("employe_voirnomB", ['nom' => $nom]);
+  }
+  
+  
   /**
    * @Route("employe/{id}", name="voir", defaults={"id":99}, requirements= {"id":"\d+"})
    */
@@ -20,27 +39,58 @@ class EmployeController extends AbstractController
         'id' => $id
     ]);
   }
-  
-  
+
   /**
    * @Route("employeV2/{id}", name="voirV2")
    * 
+   * @Template("employe/voirEmploye.html.twig")
    */
-  public function voirEmployeV2(int $id): Response
+  public function voirEmployeV2(int $id)
   {
-    return $this->render('employe/voirEmploye.html.twig', [
-        'id' => $id
-    ]);
+    return array('id' => $id);
   }
-  
+
   /**
-   * @Route("employe/{nom}", name="voirNom", requirements= {"nom":"^[B][a-zé-î]+"})
+   * @Route("employe/{nom}", name="voirnomB", requirements= {"nom":"^[B][a-zØ-öø-ÿ]+"},
+   * options = { "utf8": true})
    * 
    */
-  public function voirNom(string $nom): Response
+  public function voirNom(string $nom)
   {
     return $this->render('employe/voirNom.html.twig', [
         'nom' => $nom
     ]);
   }
+
+  
+  /**
+   * @Route("monCV/{_locale}",
+   * name="voirCV",
+   * requirements={"_locale": "fr|en"},
+   * defaults={"_locale":"fr"})
+   */
+  public function voirCV(){
+    
+    $html = $this->render("employe/voirCV.html.twig");
+    $dompdf = new Dompdf();
+    $dompdf->loadHtml($html);
+    $dompdf->render();
+    return new Response($dompdf->stream());
+    
+    
+  }
+ /**
+   * @Route("employe/{nom}",
+   * name="employe_redirection",
+   * requirements = {"nom":"[A-Za-z]+"})
+   * 
+   */
+  public function redirection(string $nom)
+  {
+    $nom = "Bond";
+    $url = $this->generateUrl("employe_voirnomB", array('nom' => $nom));
+    return $this->redirect($url);
+  }
+  
+  
 }
